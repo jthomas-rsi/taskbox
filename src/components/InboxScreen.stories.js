98150fe6e08b1458f0e039ccd330e3f23e7fd41e
James F. Thomas <jthomas@rsimail.com>
2022-09-22 15:03:21 -0500

    created InboxScreen story with redux Provider

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import InboxScreen from "./InboxScreen";
+
+import store from "../library/store";
+
+import { Provider } from "react-redux";
+
+// define the exported story Meta object
+export default {
+    componet: InboxScreen,
+    title: 'InboxScreen',
+    decorators: [ (story) => <Provider store={store} >{story()}</Provider> ]
+};
+
+const Template = () => <InboxScreen />;
+
+export const Default = Template.bind({})
+export const Error = Template.bind({})
