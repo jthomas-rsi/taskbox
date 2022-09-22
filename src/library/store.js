18d0fc17faa53abee76e16dc5348fa8a6d366801
James F. Thomas <jthomas@rsimail.com>
2022-09-22 14:28:44 -0500

    extended reducer for async fetch ability

--- src/library/store.js
+++ src/library/store.js
@@ -1,8 +1,25 @@
 // This is the redux store that will wire in the data needed to populate of components in the story
 //-- The file contains the store / actions / reducer which would be in different files in larger app
 
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+/** Explanation of Redux Toolkit functions ^^^
+ -> configureStore:
+ sets up a well-configured Redux store with a single function call, including combining reducers, 
+ adding the thunk middleware, and setting up the Redux DevTools integration. It also is easier to
+ configure than createStore, because it takes named options parameters
+ 
+ -> createSlice:
+ lets you write reducers that use the Immer library to enable writing immutable updates using
+ "mutating" JS syntax like state.value = 123, with no spreads needed. It also automatically
+ generates action creator functions for each reducer, and generates action type strings 
+ internally based on your reducer's names.
+ 
+ ->createAsyncThunk:
+ The redux-thunk middleware, which allows simple asynchronous use of dispatch.
+
+ 
+ */
 // define the default tasks that will be displayed when the app/component loads
 const defaultTasks = [
     { id:'1', title: 'Learn Storybook', state: 'TASK_INBOX' },
@@ -17,6 +34,22 @@ const TaskBoxData = {
     error: null
 };
 
+// define async function to fetch tasks from remote endpoint
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async()=>{
+    const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos?userId=1'
+    );
+
+    const data = await response.json();
+    const result = data.map((task)=>({
+        id:`${task}`,
+        title: task.title,
+        state: task.completed ? 'TASK_ARCHIVED': 'TASK_INBOX',
+    }));
+
+    return result;
+})
+
 // define the store for the components to retrieve data from
 const TasksSlice = createSlice({
     name: 'taskbox',
@@ -29,6 +62,27 @@ const TasksSlice = createSlice({
                 state.tasks[task].state = newTaskState;
             }
         },
+    },
+    
+    // Extend the reducer used in the async function
+    extraReducers( builder ) {
+        builder
+        .addCase(fetchTasks.pending, ( state ) => {
+            state.status = 'loading';
+            state.error = null;
+            state.tasks = [];
+        })
+        .addCase(fetchTasks.fulfilled, ( state, action )=>{
+            state.status = 'succeeded';
+            state.error = null;
+            state.tasks = action.payload; //adds any fetched tasks to the tasks []
+        })
+        .addCase( fetchTasks.rejected, ( state )=> {
+            state.status = 'failed';
+            state.error = "Something went wrong";
+            state.tasks = [];
+        } );
+        
     },
 }); 
 
