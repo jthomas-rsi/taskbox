95d656612e91a236e1c6a2c39700a0c4207ff517
James F. Thomas <jthomas@rsimail.com>
2022-09-23 11:11:53 -0500

    + interaction testing to InboxScreen default story

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -15,6 +15,15 @@ import { MockedState } from './TaskList.stories';
 //import Provider from redux
 import { Provider } from "react-redux";
 
+//import tools for component interaction testing 
+import {
+    fireEvent,
+    within,
+    waitFor,
+    waitForElementToBeRemoved,
+
+} from '@storybook/testing-library'
+
 // define the exported story Meta object
 export default {
     component: InboxScreen,
@@ -36,6 +45,23 @@ Default.parameters = {
             )
         ]
     }
+}
+
+//add interaction to Default story 
+Default.play = async ({ canvasElement }) => {
+    //By default, each interaction you write inside your play function will be executed starting from the top-level element of the Canvas.
+    const canvas = within(canvasElement);
+
+    //await the component transition out of loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId('loading'))
+    
+    //await the component being updated via store 
+    await waitFor(async() => {
+        //simulate user pinning 1st task
+        await fireEvent.click(canvas.getByLabelText('pinTask-1'))
+        //simulate user pinning 3rd task
+        await fireEvent.click(canvas.getByLabelText('pinTask-3'))
+    })
 }
 
 export const Error = Template.bind({})
