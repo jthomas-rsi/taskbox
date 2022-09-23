2f2ecb6a9b660c3d52756bd385e4070afee10dee
James F. Thomas <jthomas@rsimail.com>
2022-09-23 10:34:16 -0500

    + msw package and file to public folder

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import '../src/index.css'
 
+// register Mock Service Worker _ MSW addon 
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+//initialize the msw addon 
+initialize();
+
+//export decorators for global use
+export const decorators = [mswDecorator];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
